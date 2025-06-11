7ca40fb54ee8526ec7259ab8fff3ffc070a2f8ee
lucasb <lkab05@hotmail.com>
2025-06-11 20:18:02 -0300

    feat: create category use case abstraction and output expected

---
+++ catalog-admin/application/src/main/java/com/github/lucasbarbosaalves/catalog/application/category/create/CreateCategoryUseCase.java
@@ -0,0 +1,8 @@
+package com.github.lucasbarbosaalves.catalog.application.category.create;
+
+import com.github.lucasbarbosaalves.catalog.application.UseCase;
+
+public abstract class CreateCategoryUseCase extends UseCase<CreateCategoryCommand, CreateCategoryOutput> {
+
+
+}
