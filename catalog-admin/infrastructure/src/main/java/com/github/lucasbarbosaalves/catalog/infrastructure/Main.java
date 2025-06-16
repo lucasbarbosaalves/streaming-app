ba77bd090c3b71d5f5feef17165dd84c479b8090
lucasb <lkab05@hotmail.com>
2025-06-16 19:29:37 -0300

    feat: set up Spring Boot application with Undertow server and initial configuration

---
+++ catalog-admin/infrastructure/src/main/java/com/github/lucasbarbosaalves/catalog/infrastructure/Main.java
@@ -0,0 +1,16 @@
+package com.github.lucasbarbosaalves.catalog.infrastructure;
+
+import com.github.lucasbarbosaalves.catalog.infrastructure.config.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+//      System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "development");
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
+        SpringApplication.run(WebServerConfig.class, args);
+
+    }
+}
---
+++ catalog-admin/infrastructure/src/main/java/com/github/lucasbarbosaalves/catalog/infrastructure/config/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.github.lucasbarbosaalves.catalog.infrastructure.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.github.lucasbarbosaalves.catalog")
+public class WebServerConfig {
+}
