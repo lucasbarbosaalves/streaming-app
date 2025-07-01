f4ffe5682156e962df034c21dfe64afab8ed8f2d
lucasb <lkab05@hotmail.com>
2025-07-01 16:52:07 -0300

    refactor: enhance cleanup logic in test extensions for genre and category repositories

--- catalog-admin/infrastructure/src/test/java/com/github/lucasbarbosaalves/catalog/CleanUpExtensions.java
+++ catalog-admin/infrastructure/src/test/java/com/github/lucasbarbosaalves/catalog/CleanUpExtensions.java
@@ -1,18 +1,30 @@
 package com.github.lucasbarbosaalves.catalog;
 
+import com.github.lucasbarbosaalves.catalog.infrastructure.category.persistence.CategoryRepository;
+import com.github.lucasbarbosaalves.catalog.infrastructure.genre.persistence.GenreRepository;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Collection;
+import java.util.List;
 
 class CleanUpExtensions implements BeforeEachCallback {
-    @Override
-    public void beforeEach(ExtensionContext context) throws Exception {
-        var repositories = SpringExtension.getApplicationContext(context).getBeansOfType(CrudRepository.class).values();
 
-        cleanUp(repositories);
+    @Override
+    public void beforeEach(ExtensionContext context) {
+        final var applicationContext = SpringExtension.getApplicationContext(context);
+
+        cleanUp(List.of(
+                applicationContext.getBean(GenreRepository.class),
+                applicationContext.getBean(CategoryRepository.class)
+        ));
+
+        final var em = applicationContext.getBean(TestEntityManager.class);
+        em.flush();
+        em.clear();
     }
 
     private void cleanUp(final Collection<CrudRepository> repositories) {
--- catalog-admin/infrastructure/src/test/java/com/github/lucasbarbosaalves/catalog/MySQLGatewayTest.java
+++ catalog-admin/infrastructure/src/test/java/com/github/lucasbarbosaalves/catalog/MySQLGatewayTest.java
@@ -14,6 +14,7 @@ import java.lang.annotation.*;
 @ActiveProfiles("test")
 @DataJpaTest
 @ComponentScan(
+        basePackages = "com.github.lucasbarbosaalves.catalog",
         includeFilters = {
                 @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".*MySQLGateway")
         }
