c0307332b7a0c96e66b38a65a75bc7eae9973072
lucasb <lkab05@hotmail.com>
2025-07-07 14:10:34 -0300

    refactor: updated GenreAPI and GenreController to support UpdateGenreRequest and improve API documentation

--- catalog-admin/infrastructure/src/main/java/com/github/lucasbarbosaalves/catalog/infrastructure/api/GenreAPI.java
+++ catalog-admin/infrastructure/src/main/java/com/github/lucasbarbosaalves/catalog/infrastructure/api/GenreAPI.java
@@ -5,11 +5,13 @@ import com.github.lucasbarbosaalves.catalog.domain.pagination.Pagination;
 import com.github.lucasbarbosaalves.catalog.infrastructure.genre.models.CreateGenreRequest;
 import com.github.lucasbarbosaalves.catalog.infrastructure.genre.models.GenreListResponse;
 import com.github.lucasbarbosaalves.catalog.infrastructure.genre.models.GenreResponse;
+import com.github.lucasbarbosaalves.catalog.infrastructure.genre.models.UpdateGenreRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,8 +20,8 @@ import org.springframework.web.bind.annotation.*;
 public interface GenreAPI {
 
     @PostMapping(
-            consumes = "application/json",
-            produces = "application/json"
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
     )
     @Operation(summary = "Create a new genre")
     @ApiResponses(value = {
@@ -44,35 +46,41 @@ public interface GenreAPI {
             @RequestParam(name = "dir", required = false, defaultValue = "asc") final String direction
     );
 
-    @GetMapping(value = "{id}", produces = "application/json")
-    @Operation(summary = "Get a genre by ID")
+    @GetMapping(
+            value = "{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @Operation(summary = "Get a genre by it's identifier")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Genre found successfully"),
-            @ApiResponse(responseCode = "404", description = "Genre not found"),
+            @ApiResponse(responseCode = "200", description = "Genre retrieved successfully"),
+            @ApiResponse(responseCode = "404", description = "Genre was not found"),
             @ApiResponse(responseCode = "500", description = "An internal server error was thrown"),
     })
-    GenreResponse getById(@PathVariable("id") String id);
+    GenreResponse getById(@PathVariable(name = "id") String id);
 
-    @PutMapping(value = "{id}", produces = "application/json")
-    @Operation(summary = "Update a genre by ID")
+    @PutMapping(
+            value = "{id}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @Operation(summary = "Update a genre by it's identifier")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Genre updated successfully"),
-            @ApiResponse(responseCode = "404", description = "Genre not found"),
-            @ApiResponse(responseCode = "422", description = "A validation error was thrown"),
+            @ApiResponse(responseCode = "404", description = "Genre was not found"),
             @ApiResponse(responseCode = "500", description = "An internal server error was thrown"),
     })
-    ResponseEntity<?> updateById(
-            @PathVariable("id") String id,
-            @RequestBody CreateGenreRequest input
-    );
+    ResponseEntity<?> updateById(@PathVariable(name = "id") String id, @RequestBody UpdateGenreRequest input);
 
-    @DeleteMapping(value = "{id}", produces = "application/json", consumes = "application/json")
+    @DeleteMapping(
+            value = "{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @Operation(summary = "Delete a genre by ID")
+    @Operation(summary = "Delete a genre by it's identifier")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "204", description = "Genre deleted successfully"),
-            @ApiResponse(responseCode = "404", description = "Genre not found"),
+            @ApiResponse(responseCode = "404", description = "Genre was not found"),
             @ApiResponse(responseCode = "500", description = "An internal server error was thrown"),
     })
-    void deleteById(@PathVariable("id") String id);
+    void deleteById(@PathVariable(name = "id") String id);
 }
--- catalog-admin/infrastructure/src/main/java/com/github/lucasbarbosaalves/catalog/infrastructure/api/controllers/GenreController.java
+++ catalog-admin/infrastructure/src/main/java/com/github/lucasbarbosaalves/catalog/infrastructure/api/controllers/GenreController.java
@@ -15,6 +15,7 @@ import com.github.lucasbarbosaalves.catalog.infrastructure.api.GenreAPI;
 import com.github.lucasbarbosaalves.catalog.infrastructure.genre.models.CreateGenreRequest;
 import com.github.lucasbarbosaalves.catalog.infrastructure.genre.models.GenreListResponse;
 import com.github.lucasbarbosaalves.catalog.infrastructure.genre.models.GenreResponse;
+import com.github.lucasbarbosaalves.catalog.infrastructure.genre.models.UpdateGenreRequest;
 import com.github.lucasbarbosaalves.catalog.infrastructure.genre.presenters.GenreApiPresenter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,54 +44,51 @@ public class GenreController implements GenreAPI {
     }
 
     @Override
-    public ResponseEntity<?> create(CreateGenreRequest input) {
-        final var command = CreateGenreCommand.with(
+    public ResponseEntity<?> create(final CreateGenreRequest input) {
+        final var aCommand = CreateGenreCommand.with(
                 input.name(),
                 input.isActive(),
                 input.categories()
         );
 
-        CreateGenreOutput output = this.createGenreUseCase.execute(command);
+        final var output = this.createGenreUseCase.execute(aCommand);
 
         return ResponseEntity.created(URI.create("/genres/" + output.id())).body(output);
     }
 
     @Override
-    public Pagination<GenreListResponse> list(String search, int page, int perPage, String sort, String direction) {
-        final var query = this.listGenreUseCase.execute(
-                new SearchQuery(
-                        page,
-                        perPage,
-                        search,
-                        sort,
-                        direction
-                )
-        );
-
-        return query.map(GenreApiPresenter::present);
+    public Pagination<GenreListResponse> list(
+            final String search,
+            final int page,
+            final int perPage,
+            final String sort,
+            final String direction
+    ) {
+        return this.listGenreUseCase.execute(new SearchQuery(page, perPage, search, sort, direction))
+                .map(GenreApiPresenter::present);
     }
 
     @Override
-    public GenreResponse getById(String id) {
+    public GenreResponse getById(final String id) {
         return GenreApiPresenter.present(this.getGenreByIdUseCase.execute(id));
     }
 
     @Override
-    public ResponseEntity<?> updateById(String id, CreateGenreRequest input) {
-        final var command = UpdateGenreCommand.with(
+    public ResponseEntity<?> updateById(final String id, final UpdateGenreRequest input) {
+        final var aCommand = UpdateGenreCommand.with(
                 id,
                 input.name(),
                 input.isActive(),
                 input.categories()
         );
 
-        UpdateGenreOutput output = this.updateGenreUseCase.execute(command);
+        final var output = this.updateGenreUseCase.execute(aCommand);
 
         return ResponseEntity.ok(output);
     }
 
     @Override
-    public void deleteById(String id) {
+    public void deleteById(final String id) {
         this.deleteGenreUseCase.execute(id);
     }
 }
