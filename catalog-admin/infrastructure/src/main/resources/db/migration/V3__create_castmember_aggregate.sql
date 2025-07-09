efb0396b71234570fc59ec59d2e5f374159b3f1e
lucasb <lkab05@hotmail.com>
2025-07-09 15:46:09 -0300

    feat: created flyway migration - cast_members table

---
+++ catalog-admin/infrastructure/src/main/resources/db/migration/V3__create_castmember_aggregate.sql
@@ -0,0 +1,9 @@
+CREATE TABLE cast_members
+(
+    id         VARCHAR(36) NOT NULL PRIMARY KEY,
+    name       VARCHAR(255) NOT NULL,
+    type       VARCHAR(32) NOT NULL,
+    created_at datetime(6)  NOT NULL,
+    updated_at datetime(6)  NOT NULL
+);
+
